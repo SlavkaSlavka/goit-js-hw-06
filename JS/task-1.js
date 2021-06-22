9a97204c211da1c342262b55044a04856042937c
SlavkaSlavka <slavkabest1993@gmail.com>
2021-06-22 21:27:53 +0300

    last task

--- JS/task-1.js
+++ JS/task-1.js
@@ -83,9 +83,10 @@ console.log(getNamesSortedByFriendsCount(users));
 // Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся 
 // умений и они должны быть отсортированы в алфавитном порядке.
 
-const getSortedUniqueSkills = users => {
-
-};
+const getSortedUniqueSkills = users => users.flatMap(user => user.skills)
+    .filter((skill, index, arr) => arr.indexOf(skill) === index)
+    .sort();
+ 
 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
